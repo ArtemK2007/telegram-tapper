9231f6e4cbcbfa5e4251f41923efb53c575e7efa
Artem <artemkryachko2007@gmail.com>
2025-11-12 23:47:02 +0100

    Fix export error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-// src/main.jsx
+import React from 'react';
 
-// Инициализация Telegram Web App
-if (window.Telegram && window.Telegram.WebApp) {
-  window.Telegram.WebApp.ready();
-  window.Telegram.WebApp.expand();
+function App() {
+  return (
+    <div className="soon-text">
+      SOON v2
+    </div>
+  );
 }
-// Остальной код файла main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // Здесь могут быть глобальные стили, влияющие на body
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default App;
